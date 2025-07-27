8a851c4df47efe72697d5f8e578f80b52a571f5d
Anurag Wazarkar <wazarkaranurag@gmail.com>
2025-07-27 22:06:49 +0530

    adding mobile hook

---
+++ hooks/use-mobile.ts
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export function useIsMobile() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsMobile(window.innerWidth <= 768);
+    }
+    handleResize(); // Set initial value
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return isMobile;
+} 
